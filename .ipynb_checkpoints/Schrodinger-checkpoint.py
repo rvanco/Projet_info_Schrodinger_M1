56a9a26fdf43e9e063c1a2df68104e2ad412bdf0
Benjamin Bakri <benjamin.bakri@etu.unistra.fr>
2022-02-09 17:44:42 +0100

    Methode Mariane
    crible des valeurs de E et filtre sur les fonctions d'ondes convergentes

---
+++ .ipynb_checkpoints/Schrodinger-checkpoint.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  2 16:24:17 2022
+
+@author: renaudvanco
+"""
+#modif faite dans la branche ben1
+
+import numpy as np
+import matplotlib.pyplot as plt
